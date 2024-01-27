7fc4e17de7033a37684fe6a5f232538ac0a13645
ESCM1823 <cambranesstevens@gmail.com>
2024-01-26 19:04:13 -0600

    Actualizacion de archivos

--- Tarea2/VarHolaMundo.py
+++ Tarea2/VarHolaMundo.py
@@ -1,5 +1,8 @@
 #0901-22-940-Edwin-Stevens-Cambranes-Mendizábal
 
 HolaMundo = "¡HOLA MUNDO SOY STEVENS CAMBRANES!" #crear y asignar valor a una variable
+
+#aqui estoy usando metodo lower() que esto hace que la cadena de texto mayuscula se convierta en minusculas
 salida = HolaMundo.lower()
+
 print("El contenido de la variable es: ",salida) #imprimir por consola la cadena
