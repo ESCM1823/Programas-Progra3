572d66aabc771a3262980c6976934cbd066aa26a
ESCM1823 <cambranesstevens@gmail.com>
2024-02-09 22:11:05 -0600

    Actualizacion archivos .py

--- Tarea4/revertirMitadCola.py
+++ Tarea4/revertirMitadCola.py
@@ -5,30 +5,32 @@ La cola debe mantener la misma secuencia de elementos, pero la mitad debe estar 
 '''
 from collections import deque
 
-def revertirMitad(cola):
+def revertir_mitad_cola(cola):
     pila = []
 
     longitud = len(cola)
     mitad = longitud // 2
     
-    #simplemente indica que estamos iterando mitad veces, pero no estamos utilizando los valores del índice en el bucle. 
-
     # Transferir la mitad de la cola a la pila
     for _ in range(mitad):
         pila.append(cola.popleft())
     
     # Transferir la mitad de la pila a la cola en orden inverso
     for _ in range(mitad):
-        cola.appendleft(pila.pop())
+        pila.append(cola.pop())
 
     # Si la longitud de la cola es impar, omitir el elemento central
     if longitud % 2 != 0:
-        cola.append(cola.popleft())
+        cola.append(pila.pop())
     
+    # Volver a colocar los elementos en la cola en el orden correcto
+    while pila:
+        cola.appendleft(pila.pop())
+
     return cola
 
 # Datos de ejemplo
-enCola = deque([1, 2, 3, 4, 5, 6, 7, 8, 9])
-print("Cola original:", enCola)
-revertirMitad(enCola)
-print("Cola con la mitad revertida:", enCola)
+en_cola = deque([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print("Cola original:", en_cola)
+en_cola = revertir_mitad_cola(en_cola)
+print("Cola con la mitad revertida:", en_cola)
